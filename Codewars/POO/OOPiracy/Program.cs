921d9f81f700074a10db2d00c4a525dbc6809255
Seu Nome <cgouveiaigor@gmail.com>
2025-07-23 11:51:45 -0300

    OOP problem: OOPiracy

---
+++ Codewars/POO/OOPiracy/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Ship
+{
+    public int Draft;
+    public int Crew;
+
+    public Ship(int draft, int crew)
+    {
+        Draft = draft;
+        Crew = crew;
+    }
+
+    public bool IsWorthIt()
+    {
+        int tempD = Draft;
+        double eachMate = 1.5 * Crew;
+        double worth = tempD - eachMate;
+
+        if (worth > 20) return true;
+
+
+
+        return false;
+
+    }
+    
+}
