3d7006f42e8f74d52cd8fb5e2a3326a503982060
Seu Nome <cgouveiaigor@gmail.com>
2025-07-29 11:21:19 -0300

    OOP problem: HeroClass

---
+++ Codewars/POO/HeroClass/Program.cs
@@ -0,0 +1,20 @@
+﻿// It's dangerous to go alone... create a Hero class to accompany you on your quest!
+
+public class Hero
+{
+    public string Name;
+    public string Position;
+    public float Health;
+    public float Damage;
+    public int Experience;
+
+    public Hero(string nome = "Hero", string pos = "00", float saude = 100, float dano = 5, int xp = 0)
+    {
+        Name = nome;
+        Position = pos;
+        Health = saude;
+        Damage = dano;
+        Experience = xp;
+    }
+
+}
